c70d22d083e21385fcc925d22e2deb580392c4eb
Kim Alvefur <zash@zash.se>
2012-09-08 17:53:39 +0200

    squish.sh: Add runtime check for struct

--- squish.sh
+++ squish.sh
@@ -7,6 +7,10 @@ Module() {
 
 echo 'if not pcall(require,"ffi") then return end'
 Module "net.adns" "net.ldns.lua"
+echo 'if pcall(require, "struct") then'
+Module "util.dns" "util.dns+struct.lua"
+echo 'else'
 Module "util.dns" "util.dns.lua"
+echo 'end'
 Module "lib.unbound" "lib.unbound.lua"
 Module "net.dns" "fakedns.lua"
