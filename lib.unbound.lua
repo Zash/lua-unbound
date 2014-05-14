9f6106df6daf9339497b2cff2aedea76f1e5491c
Kim Alvefur <zash@zash.se>
2014-05-14 15:22:39 +0200

    lib.unbound: Remove some unused C defininitions

--- lib.unbound.lua
+++ lib.unbound.lua
@@ -3,8 +3,8 @@
 --
 -- This file is MIT/X11 licensed.
 
-local setmetatable = setmetatable;
 local ffi = require "ffi";
+local jit = require "jit";
 local char = ffi.new("char *");
 local function tochar(s)
 	return ffi.cast(char, s);
@@ -32,17 +32,9 @@ struct ub_result {
 typedef void (*ub_callback_t)(void*, int, struct ub_result*);
 struct ub_ctx* ub_ctx_create(void);
 void ub_ctx_delete(struct ub_ctx* ctx);
-int ub_ctx_set_option(struct ub_ctx* ctx, char* opt, char* val);
-int ub_ctx_get_option(struct ub_ctx* ctx, char* opt, char** str);
-int ub_ctx_config(struct ub_ctx* ctx, char* fname);
-int ub_ctx_set_fwd(struct ub_ctx* ctx, char* addr);
 int ub_ctx_resolvconf(struct ub_ctx* ctx, char* fname);
 int ub_ctx_hosts(struct ub_ctx* ctx, char* fname);
 int ub_ctx_add_ta(struct ub_ctx* ctx, char* ta);
-int ub_ctx_add_ta_file(struct ub_ctx* ctx, char* fname);
-int ub_ctx_trustedkeys(struct ub_ctx* ctx, char* fname);
-int ub_ctx_debugout(struct ub_ctx* ctx, void* out);
-int ub_ctx_debuglevel(struct ub_ctx* ctx, int d);
 int ub_ctx_async(struct ub_ctx* ctx, int dothread);
 int ub_poll(struct ub_ctx* ctx);
 int ub_wait(struct ub_ctx* ctx);
@@ -55,11 +47,6 @@ int ub_resolve_async(struct ub_ctx* ctx, char* name, int rrtype,
 int ub_cancel(struct ub_ctx* ctx, int async_id);
 void ub_resolve_free(struct ub_result* result);
 const char* ub_strerror(int err);
-int ub_ctx_print_local_zones(struct ub_ctx* ctx);
-int ub_ctx_zone_add(struct ub_ctx* ctx, char *zone_name, char *zone_type);
-int ub_ctx_zone_remove(struct ub_ctx* ctx, char *zone_name);
-int ub_ctx_data_add(struct ub_ctx* ctx, char *data);
-int ub_ctx_data_remove(struct ub_ctx* ctx, char *data);
 const char* ub_version(void);
 ]];
 local unbound = { _LIBVER = ffi.string(libunbound.ub_version()), _LIB = libunbound };
